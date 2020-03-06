c1608826260d908fb371a1bca83136e956ff0222
Yaroslav Panteleev <31574114+yarikpythonist@users.noreply.github.com>
2020-03-06 23:29:08 +0300

    Add files via upload

---
+++ Image.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using byte = unsigned char;
+using big = unsigned long long int;
+
+struct Pixel {
+	byte r;
+	byte g;
+	byte b;
+};
+
+int to_int(std::vector <char>& v);
+
+class Image {
+public:
+	void set_vars(char* in, char* out);
+	void read(char* in);
+	void read();
+	void write(char* out);
+	void write();
+	void inversion();
+	void vertical_display();
+	void horizontal_display();
+	void rotate(bool type);
+	~Image();
+protected:
+	void _read(char* in);
+	void _write(char* out);
+private:
+	char* in;
+	char* out;
+	FILE* file_in;
+	FILE* file_out;
+	char mode[3]; // P5 or P6
+	std::vector <char> _w, _h, _d;
+	size_t w, h, d; // width, heigth and color depth
+	std::vector <std::vector <byte>> bnw; // for P5
+	std::vector <std::vector <Pixel>> rgb; // for P6
+};
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Image.h"
+using std::cout;
+using std::cin;
+
+int main(int argc, char* argv[]) {
+	Image img1;
+	if (argc != 4) {
+		cout << "Unsupported count of arguments\n";
+		exit(1);
+	}
+	img1.set_vars(argv[1], argv[2]);
+	img1.read();
+	if (argv[3][0] == '0')
+		img1.inversion();
+	if (argv[3][0] == '1')
+		img1.horizontal_display();
+	if (argv[3][0] == '2')
+		img1.vertical_display();
+	if (argv[3][0] == '3')
+		img1.rotate(0);
+	if (argv[3][0] == '4')
+		img1.rotate(1);
+	img1.write();
+	return 0;
+}
